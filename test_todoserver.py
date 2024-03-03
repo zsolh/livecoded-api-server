40bc2e520e03466720cd36952a90b2cd317f3b5f
zsolh <holman.zsolt@gmail.com>
2024-03-03 01:44:13 +0100

    Cycle of creating a task and fetching its details

--- test_todoserver.py
+++ test_todoserver.py
@@ -1,12 +1,42 @@
+# test_todoserver.py
 import unittest
 import json
-
 from todoserver import app
+app.testing = True
+
+def json_body(resp):
+    return json.loads(resp.data.decode("utf-8"))
 
 class TestTodoserver(unittest.TestCase):
     def test_get_empty_list_of_tasks(self):
         client = app.test_client()
         resp = client.get("/tasks/")
         self.assertEqual(200, resp.status_code)
-        data = json.loads(resp.data.decode("utf-8"))
-        self.assertEqual([], data)
+        self.assertEqual([], json_body(resp))
+
+    def test_create_a_task_and_get_its_details(self):
+        client = app.test_client()
+        # verify test pre-conditions
+        resp = client.get("/tasks/")
+        self.assertEqual([], json_body(resp))
+        # create new task
+        new_task_data = {
+            "summary": "Get milk",
+            "description": "One gallon organic whole milk",
+        }
+        resp = client.post("/tasks/",
+                           data=json.dumps(new_task_data))
+        self.assertEqual(201, resp.status_code)
+        data = json_body(resp)
+        self.assertIn("id", data)
+        # get task details
+        task_id = data["id"]
+        resp = client.get("/tasks/{:d}/".format(task_id))
+        self.assertEqual(200, resp.status_code)
+        task = json_body(resp)
+        self.assertEqual(task_id, task["id"])
+        self.assertEqual("Get milk", task["summary"])
+        self.assertEqual("One gallon organic whole milk",
+                         task["description"])
+
+
--- todoserver.py
+++ todoserver.py
@@ -1,10 +1,30 @@
-from flask import Flask, make_response
+from flask import Flask, make_response, request
+import json
 
 app = Flask(__name__)
 
-@app.route("/tasks/")
+MEMORY = {}
+
+@app.route("/tasks/", methods=["GET"])
 def get_all_tasks():
     return make_response("[]", 200)
+
+@app.route("/tasks/", methods=["POST"])
+def create_task():
+    payload = request.get_json(force = True)
+    task_id = 1
+    MEMORY[task_id] = {
+            "summary" : payload["summary"],
+            "description":payload["description"],
+            }
+    data = {"id" : task_id}
+    return make_response(json.dumps(data), 201)
+
+@app.route("/tasks/<int:task_id>/")
+def task_details(task_id):
+    task_info = MEMORY[task_id].copy()
+    task_info["id"] = task_id
+    return json.dumps(task_info)
 
 if __name__ == "__main__":
     app.run()
